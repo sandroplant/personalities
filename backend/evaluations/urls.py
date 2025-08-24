ceac0dfd965c628ba05b3194cb909a34c4e7d536
sandroplant <sandroplant@gmail.com>
2025-08-24 16:33:32 -0700

    Add tasks endpoint route to evaluations urls

--- backend/evaluations/urls.py
+++ backend/evaluations/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CriterionListCreateView, EvaluationListCreateView
-
+from .views import CriterionListCreateView, EvaluationListCreateView, EvaluationTasksView
 
 urlpatterns = [
     path('criteria/', CriterionListCreateView.as_view(), name='criteria-list-create'),
     path('evaluations/', EvaluationListCreateView.as_view(), name='evaluation-list-create'),
+    path('tasks/', EvaluationTasksView.as_view(), name='evaluation-tasks'),
 ]
